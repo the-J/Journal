981fc787b8ef70445293aa2b0b323b3f19e45dd0
Juliusz Jakubowski <asfalt09@gmail.com>
2018-06-27 09:42:01 +0200

    Post.js init

---
+++ journal/Post.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Post extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Post.js </Text>
+      </View>
+    );
+  }
+}
