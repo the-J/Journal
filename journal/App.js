ca9ac570c05c300c0e79f167df4216d16127e04a
Juliusz Jakubowski <asfalt09@gmail.com>
2018-06-27 08:44:06 +0200

    setting prettier

--- journal/App.js
+++ journal/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { StackNavigator } from "react-navigation";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 export default class App extends React.Component {
   render() {
@@ -15,8 +15,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
