3e749f7b6b0a863bbcdfb436f14f463c64956f76
Juliusz Jakubowski <asfalt09@gmail.com>
2018-06-29 08:07:18 +0200

    setup apollo client

--- journal/App.js
+++ journal/App.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
+// Apollo
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
 import navStyles from './styles/navStyles';
 
 import Post from './Post';
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: 'https://api.graph.cool/simple/v1/cjiy4mzjl242e0157keehyxqf',
+      credentials: 'same-origin'
+    })
+  ]),
+  cache: new InMemoryCache()
+});
 
 class App extends Component {
   static navigationOptions = { title: 'Home', ...navStyles };
@@ -13,11 +40,13 @@ class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Hello</Text>
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <Text> Hello</Text>
 
-        <Button onPress={this.goToPost} title="Go to post page" />
-      </View>
+          <Button onPress={this.goToPost} title="Go to post page" />
+        </View>
+      </ApolloProvider>
     );
   }
 }
