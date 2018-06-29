c108bfc54be0f8268f6cbfafc65b57dc2afa2b4e
Juliusz Jakubowski <asfalt09@gmail.com>
2018-06-29 10:05:36 +0200

    creatigng single post quey for handling entry to Post

--- journal/Post.js
+++ journal/Post.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import navStyles from './styles/navStyles';
 
-export default class Post extends Component {
-  static navigationOptions = {
-    title: 'Post',
-    ...navStyles
-  };
+class Post extends Component {
+  static navigationOptions = { title: 'Post', ...navStyles };
 
   render() {
-    console.log(this.props);
+    const { Post, loading } = this.props;
+
+    if (loading) return null;
+
     return (
       <View>
-        <Text> {this.props.navigation.state.params.id} </Text>
+        <Text>{this.props.Post.id}</Text>
+        <Text>{this.props.Post.title}</Text>
       </View>
     );
   }
 }
+
+const postQuery = gql`
+  query Post($id: ID!) {
+    Post(id: $id) {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(postQuery, {
+  props: ({ data }) => ({ ...data }),
+  options: ({ navigation }) => ({
+    variables: {
+      id: navigation.state.params.id
+    }
+  })
+})(Post);
