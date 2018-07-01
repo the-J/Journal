cfa974d70783252de070a91a052a1f9de86bd994
Juliusz Jakubowski <asfalt09@gmail.com>
2018-07-01 11:33:46 +0200

    utilities f orhandling token  operations

---
+++ journal/utils/util-login.js
@@ -0,0 +1,21 @@
+import { AsyncStorage } from 'react-native';
+
+let token;
+
+export const getToken = async () => {
+  if (token) {
+    return Promise.resolve(token);
+  }
+
+  token = await AsyncStorage.getItem('AUTH_TOKEN');
+  return token;
+};
+
+export const signIn = newToken => {
+  return AsyncStorage.setItem('AUTH_TOKEN', newToken);
+};
+
+export const signOut = newToken => {
+  token = undefined;
+  return AsyncStorage.remove('AUTH_TOKEN');
+};
