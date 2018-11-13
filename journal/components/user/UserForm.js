070306087af139a5648fb4c225b4ad84eda39cce
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-13 16:07:54 +0100

    validating if registration data was provided

--- journal/components/user/UserForm.js
+++ journal/components/user/UserForm.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { Form, Input, Item, Label } from 'native-base';
 
 export default class UserForm extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+
+        errorEmail: '',
+        errorPass: ''
     };
 
     submitForm = () => {
         const { email, password } = this.state;
 
+        if (!email) this.setState({ errorEmail: 'Email is required' });
+        if (!password) this.setState({ errorPass: 'Password is required' });
+
+        if (!email || !password) return;
+
         this.props.onSubmit({
             email,
             password
         });
+    };
+
+    onTextChange = e => {
+        const key = Object.keys(e)[ 0 ];
+        const value = Object.values(e)[ 0 ];
+
+        if (key === 'email') {
+            this.setState({
+                email: value,
+                errorEmail: ''
+            });
+        }
+
+        if (key === 'password') {
+            this.setState({
+                password: value,
+                errorPass: ''
+            });
+        }
     };
 
     render() {
@@ -25,33 +52,42 @@ export default class UserForm extends Component {
 
                     <Input
                         keyboardType="email-address"
-                        onChangeText={email => this.setState({ email })}
+                        onChangeText={email => this.onTextChange({ email: email })}
                         value={this.state.email}
                     />
                 </Item>
+
+                {this.state.errorEmail ? <Text style={styles.error}>{this.state.errorEmail}</Text> : null}
 
                 <Item floatingLabel>
                     <Label>Password</Label>
 
                     <Input
                         secureTextEntry
-                        onChangeText={password => this.setState({ password })}
+                        onChangeText={password => this.onTextChange({ password: password })}
                         value={this.state.password}
                     />
                 </Item>
 
-                <Button
-                    style={styles.button}
-                    title={this.props.type}
-                    onPress={this.submitForm}
-                />
+                {this.state.errorPass ? <Text style={styles.error}>{this.state.errorPass}</Text> : null}
+
+                <View style={styles.button}>
+                    <Button
+                        title={this.props.type}
+                        onPress={this.submitForm}
+                    />
+                </View>
             </Form>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    body: {
-        height: 100
+    button: {
+        width: '90%',
+        margin: 10
+    },
+    error: {
+        color: 'red'
     }
 });
